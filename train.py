c43ae4f15120d99af83c97ff8d96be22943b1c05
Isaac Buitrago <mightyking7@gmail.com>
2020-05-07 23:25:19 -0500

    Saved policy to file

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 from env import GridWorld
 # from algo import Q_learning
@@ -11,7 +12,7 @@ rows = 6
 cols = 25
 
 # probability of taking a greedy action
-epsilon = 0.9
+epsilon = 0.1
 
 # discount factor
 gamma = 0.6
@@ -25,6 +26,9 @@ lr = 0.01
 # episodes for training
 n_episodes = 500
 
+policy_dir = "./policy/"
+fname_policy = "./policy/sidewalk_policy.npy"
+
 
 # 1 is an obstacle, 0 is an empty cell
 # dist = [1,0,0,0,0]
@@ -33,76 +37,94 @@ n_episodes = 500
 #
 # world = np.array([np.random.choice(dist) for i in range (num_tiles)]).reshape((rows, cols))
 
+# make dir for policy
+if not os.path.exists(policy_dir):
+    os.mkdir(policy_dir)
+
 grid_world = GridWorld(nRows = rows, nCols = cols, nA = nA, gamma = gamma)
-# behavior_policy = RandomPolicy()
+
+# train if policy doesn't exist
+if not os.path.exists(fname_policy):
+
+    # randomly initialize Q table
+    Q = np.random.rand(grid_world.nS, grid_world.nA)
+
+    # set state-action value of final states to 0
+    # TODO make this dynamic
+    Q[[24, 49, 74, 99, 124, 149], :] = 0
 
 
-# randomly initialize Q table
-Q = np.random.rand(grid_world.nS, grid_world.nA)
+    for _ in tqdm(range(n_episodes)):
 
-# set state-action value of final states to 0
-Q[[24, 49, 74, 99, 124, 149], :] = 0
+        state = grid_world.reset()
 
-for _ in tqdm(range(n_episodes)):
+        done = False
 
-    state = grid_world.reset()
+        while not done :
 
-    done = False
+            # explore action space
+            if np.random.uniform(0, 1) < epsilon:
+                action = grid_world.sample()
 
-    while not done :
+            # exploit
+            else:
+                action = np.argmax(Q[state])
 
-        # explore action space
-        if np.random.uniform(0, 1) < epsilon:
-            action = grid_world.sample()
+            # take step in env
+            obs, r, done = grid_world.step(action)
 
-        # exploit
-        else:
-            action = np.argmax(Q[state])
+            # update Q table
+            prev_value = Q[state, action]
 
-        # take step in env
-        obs, r, done = grid_world.step(action)
+            new_value = prev_value + lr * (r + grid_world.gamma * np.max(Q[obs]) - prev_value)
 
-        # update Q table
-        prev_value = Q[state, action]
+            Q[state, action] = new_value
 
-        new_value = prev_value + lr * (r + grid_world.gamma * np.max(Q[obs]) - prev_value)
+            # update state
+            state = obs
 
-        Q[state, action] = new_value
+    # save policy
+    np.save(fname_policy, Q)
 
-        # update state
-        state = obs
-
-# sidewalk policy
-# Q = Q_learning(grid_world.gamma, trajs, behavior_policy, lr = lr,
-#                initQ=np.zeros((grid_world.nS, grid_world.nA)))
+else:
+    # load policy
+    Q = np.load(fname_policy)
 
 n_runs = 1
 
 path = []
 
 # evaluate policy
-for i in range(n_runs):
-    state = grid_world.reset()
-
-    path.append(state)
-
-    done = False
-
-    while not done:
-        action = np.argmax(Q[state])
-        obs, r, done = grid_world.step(action)
-
-        state = obs
-
-        # add next state to path
-        path.append(obs)
-        print(path)
-
-print("Done")
+# for i in range(n_runs):
+#     state = grid_world.reset()
+#
+#     path.append(state)
+#
+#     done = False
+#
+#     while not done:
+#         action = np.argmax(Q[state])
+#         obs, r, done = grid_world.step(action)
+#
+#         state = obs
+#
+#         # add next state to path
+#         path.append(obs)
+#         print(path)
+#
+# print("Done")
 
 # plot policy
+si, sj = list(), list()
 
-plt.plot(0, path[0], color='red', marker='s')
-plt.plot(np.arange(1, cols), path[1:], color='blue', linestyle='--')
+for i in range(rows):
+    for j in range(cols):
+        si.append(i)
+        sj.append(j)
+
+plt.plot(sj, si, 'b.')
+# plt.plot(0, path[0], color = 'red', marker = 's')
+# plt.plot(np.arange(1, cols), path[1 : ], color = 'blue', linestyle = '--')
+
 plt.show()
 
