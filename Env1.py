7be91256136655a05ebe20401cfdfb2d6e22e182
Isaac Buitrago <mightyking7@gmail.com>
2020-05-09 01:44:14 -0500

    Created abstract class for gridworl

---
+++ Env1.py
@@ -0,0 +1,97 @@
+
+class GridWorld(object):
+    """
+    Grid world environment
+    :author: Isaac Buitrago
+    """
+    def __init__(self, nRows, nCols, nA, gamma):
+        """
+        :param nRows: number of rows in grid
+        :param nCols: number of cols in grid
+        :param nA: Number of actions
+        :param gamma: discount factor
+        """
+        self._nS = nRows * nCols
+        self._nA = nA
+        self._gamma = gamma
+        self.nRows = nRows
+        self.nCols = nCols
+
+    def terminal_states(self):
+        """
+        Generator yields terminal states
+        :return: terminal state in grid world
+        """
+        for i in range(1, self._nS + 1):
+           if i % self.nCols == 0:
+               yield i - 1
+
+    @property
+    def nS(self):
+        """ # of possible states """
+        return NotImplementedError()
+
+    @property
+    def nA(self):
+        """ # of possible actions """
+        return NotImplementedError()
+
+    @property
+    def gamma(self):
+        """ discount factor """
+        return NotImplementedError()
+
+    @property
+    def TD(self):
+        """
+        Transition Dynamics
+        return: a numpy array shape of [nS,nA,nS]
+            TD[s,a,s'] := the probability it will resulted in s' when it execute action a given state s
+        """
+        return NotImplementedError()
+
+    @property
+    def R(self):
+        """
+        Reward function
+        return: a numpy array shape of [nS,nA,nS]
+            R[s,a,s'] := reward the agent will get it experiences (s,a,s') transition.
+        """
+        return NotImplementedError()
+
+    def sample(self):
+        """
+        :return: Random action for exploration
+        """
+        return NotImplementedError()
+
+    def reset(self):
+        """
+        Reset the environment when you want to generate a new episode.
+        Randomly initializes location at beginning of sidewalk.
+        return:
+            initial state
+        """
+        return NotImplementedError()
+
+    def step(self, action:int):
+        """
+        proceed one step in the state space.
+        return:
+            next state, reward, done (whether agent reached to a terminal state)
+        """
+        return NotImplementedError()
+
+    def build_trans_mat(self):
+        """
+        Defines the transition dynamics matrix.
+        :return:
+        """
+        return NotImplementedError()
+
+    def build_reward_mat(self):
+        """
+        Defines the reward matrix
+        :return:
+        """
+        return NotImplementedError()
